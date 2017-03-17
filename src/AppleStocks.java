8795d3e82824074016db5b6e1f82d5acc28a92b8
Orphic16 <smurf016@gmail.com>
2017-03-16 20:34:29 -0500

    Initial checkin

---
+++ src/AppleStocks.java
@@ -0,0 +1,13 @@
+public class AppleStocks {
+	public static int getMaxProfit(int[] stockPricesYesterday) {
+		int maxProfit = 0;
+		int minSoFar = stockPricesYesterday[0];
+		
+		for (int i=0;i<stockPricesYesterday.length; i++) {
+			minSoFar = Math.min(stockPricesYesterday[i], minSoFar);
+			maxProfit = Math.max(stockPricesYesterday[i] - minSoFar, maxProfit);
+		}
+		
+		return maxProfit;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+	public static void main(String[] args) {
+		/*Apple Stocks
+		int[] stockPricesYesterday = new int[]{10, 7, 5, 8, 11, 9};
+		System.out.println(AppleStocks.getMaxProfit(stockPricesYesterday));*/
+		
+		
+	}
+}
