52335968e87b16ac176d375ca31e1257e5cbc420
Ryan Murphy <smurf016@gmail.com>
2021-10-25 08:47:32 -0500

    Add 1 new solution

---
+++ src/solutions/ProductOfAllOtherNumbers.java
@@ -0,0 +1,28 @@
+package solutions;
+
+public class ProductOfAllOtherNumbers {
+	public static int[] find_product(int[] numbers) {
+		int[] result = new int[numbers.length];
+
+		int[] productBeforeIndex = new int[numbers.length];
+		int[] productAfterIndex = new int[numbers.length];
+
+		int total = 1;
+		for (int i=0; i<numbers.length; i++) {
+			productBeforeIndex[i] = total;
+			total = total * numbers[i];
+		}
+
+		total = 1;
+		for (int i=numbers.length -1; i>=0; i--) {
+			productAfterIndex[i] = total;
+			total = total * numbers[i];
+		}
+
+		for (int i=0; i<numbers.length; i++) {
+			result[i] = productBeforeIndex[i] * productAfterIndex[i];
+		}
+
+		return result;
+	}
+}
---
+++ tests/ProductOfAllOtherNumbersTests.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import solutions.ProductOfAllOtherNumbers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ProductOfAllOtherNumbersTests {
+	@Test
+	public void test() {
+		int[] result = ProductOfAllOtherNumbers.find_product(new int[]{1, 7, 3, 4});
+		assertEquals(84, result[0]);
+		assertEquals(12, result[1]);
+		assertEquals(28, result[2]);
+		assertEquals(21, result[3]);
+	}
+}
